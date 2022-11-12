7f506c13ec36e38244a84e288a703665f5849451
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-12 16:18:10 -0300

    chore(ignite-dt-money): add eslint and tsconfig integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+  ],
+});
