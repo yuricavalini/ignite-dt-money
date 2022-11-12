d863668d288f6ee9cb949bf60ae83d29ca33f8e1
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-12 20:17:09 -0300

    feat(summary-component): add summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
+import Summary from '@/components/Summary';
 
 export default function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   );
 }
