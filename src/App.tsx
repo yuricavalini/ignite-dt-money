d1d5a14429f549113b76595fb86d5ffa905d460d
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-12 16:46:46 -0300

    feat(ignite-dt-money): add global style and default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  );
 }
