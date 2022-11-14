f00f6d06dc98d1dda442ae582d16c77b38121dc2
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-14 08:55:50 -0300

    feat(usesummary-hook): add useSummary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,31 +1,12 @@
-import { useContext } from 'react';
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 
-import { TransactionsContext } from '@/contexts/TransactionsContext';
 import { priceFormatter } from '@/utils/formatter';
 
+import { useSummary } from '@/hooks/useSummary';
 import { SummaryCard, SummaryContainer } from './styles';
 
 export default function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price;
-        accumulator.total += transaction.price;
-      } else {
-        accumulator.outcome += transaction.price;
-        accumulator.total -= transaction.price;
-      }
-      return accumulator;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import { TransactionsContext } from '@/contexts/TransactionsContext';
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.type === 'income') {
+        accumulator.income += transaction.price;
+        accumulator.total += transaction.price;
+      } else {
+        accumulator.outcome += transaction.price;
+        accumulator.total -= transaction.price;
+      }
+      return accumulator;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+
+  return summary;
+}
