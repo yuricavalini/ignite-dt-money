30399c20f3bba51d061a35e67f527ac411e39ab7
Yuri Cavalini <yuricavalini@gmail.com>
2022-11-14 08:46:41 -0300

    feat(transactions-page): format price and date values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import Header from '@/components/Header';
 import Summary from '@/components/Summary';
 import { TransactionsContext } from '@/contexts/TransactionsContext';
+import { dateFormatter, priceFormatter } from '@/utils/formatter';
 import SearchForm from './components/SearchForm';
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
@@ -24,11 +25,12 @@ export default function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
